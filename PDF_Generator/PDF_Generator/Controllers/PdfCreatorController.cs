578a5e61de08a7db87cecfb7796a421204958155
Marinko <marinkospasojevic@gmail.com>
2018-06-16 00:05:52 +0200

    Additional changes to create PDF on local storage

---
+++ PDF_Generator/PDF_Generator/Controllers/PdfCreatorController.cs
@@ -0,0 +1,52 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using PDF_Generator.Utility;
+using System.IO;
+
+namespace PDF_Generator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfCreatorController : ControllerBase
+    {
+        private IConverter _converter;
+
+        public PdfCreatorController(IConverter converter)
+        {
+            _converter = converter;
+        }
+        [HttpGet]
+        public IActionResult CreatePDF()
+        {
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10 },
+                DocumentTitle = "PDF Report",
+                Out = @"D:\PDFCreator\Employee_Report.pdf"
+            };
+
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = TemplateGenerator.GetHTMLString(),
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet =  Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
+                HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+                FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+            };
+
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+
+            _converter.Convert(pdf);
+
+            return Ok("Successfully created PDF document.");
+        }
+    }
+}
